d3c8c944c5be240c0e1a9b2df4e8835193f8b24d
Sharu Gowda <umeshgowda1893@gmail.com>
2022-02-23 20:55:27 -0500

    Py-Var

---
+++ 101-Python-Variables.py
@@ -0,0 +1,22 @@
+#   List of all Variables and their Usage.
+#   Python has five standard data types,
+#   Numbers,   String,  List,   Tuple,  Dictionary
+
+#   Python- Numbers
+#   Python supports four different numerical types
+
+pyint0    =   10      #   int (signed integers)
+pyint1    =   0.0     #   float (floating point real values)
+pyint2    =   3.14j   #   complex (complex numbers)
+
+#   Python Strings
+
+pystr = 'Hello World!'
+
+print (pystr)           # complete string
+print (pystr) + "HELLO" #concatenated string
+print (pystr)[0]        # first character of the string
+print (pystr) * 2       # string two times
+print (pystr)[2:4]      # characters starting from 3rd to 4th
+print (pystr)[2:]       # string starting from 3rd character
+
